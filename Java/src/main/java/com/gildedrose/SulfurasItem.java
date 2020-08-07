4af23a6fb93f0e7ac68162ff757d3192303d2589
vickyjhang <vicky.jhang@104.com.tw>
2020-08-07 16:20:58 +0800

    extract subclass for sulfuras item

---
+++ Java/src/main/java/com/gildedrose/SulfurasItem.java
@@ -0,0 +1,9 @@
+package com.gildedrose;
+
+public class SulfurasItem extends Item {
+    public SulfurasItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    public void upgrade() {}
+}
